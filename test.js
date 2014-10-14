7cb9312b5db864e8c763a60821e7cafdb93297be
Jasper St. Pierre <jstpierre@mecheye.net>
2014-10-14 10:21:46 -0700

    test: Try to run GTK+
    It fails right now due to the lack of support for inout values.

--- test.js
+++ test.js
@@ -10,3 +10,7 @@ var obj = client.query_by_device_file("/dev/dri/card0");
 console.log(obj.get_name());
 
 console.log(GLib.test_override());
+
+const Gtk = GNode.importNS("Gtk");
+Gtk.init(0, null);
+
