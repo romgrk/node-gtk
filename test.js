bd4365be607ea3a0781580d5004d38e2aac7e923
Jasper St. Pierre <jstpierre@mecheye.net>
2014-10-13 21:36:01 -0700

    Add a package.json

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 
-const gi = require('./build/Debug/gi');
-const GLib = gi.importRepo("GLib");
+const GNode = require('gnode');
+
+const GLib = GNode.importRepo("GLib");
 console.log(GLib.ascii_strup("foo", -1));
 
-const GUdev = gi.importRepo("GUdev");
+const GUdev = GNode.importRepo("GUdev");
 var client = new GUdev.Client();
 var obj = client.query_by_device_file("/dev/dri/card0");
 console.log(obj.get_name());
