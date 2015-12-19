9580e38afdaf28b64ae490f3de12c8e34c72757f
Jasper St. Pierre <jstpierre@mecheye.net>
2015-12-19 12:20:29 -0800

    Remove node-gyp-pre, and support either debug or release builds

--- node-gtk.js
+++ node-gtk.js
@@ -1,15 +1,2 @@
-var
-  npg = require('node-pre-gyp'),
-  path = require('path'),
-  gi = require(npg.find(
-    path.resolve(path.join(__dirname, 'package.json')))
-  )
-;
 
-exports.importNS = function(name, version) {
-  return gi.importNS(name, version);
-};
-
-exports.startLoop = function() {
-  gi.startLoop();
-};
+module.exports = require('./lib/node-gtk');
