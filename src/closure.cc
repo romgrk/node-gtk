7a1e2dec8e2c8901d23ceda75438748cb6d3365f
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-02-24 18:48:10 +0000

    habemus OSX

--- src/closure.cc
+++ src/closure.cc
@@ -32,13 +32,21 @@ void Closure::Marshal(GClosure *base,
     Closure *closure = (Closure *) base;
     Handle<Function> func = Handle<Function>::New(isolate, closure->persistent);
 
-    Handle<Value> argv[argc];
+    #ifdef __APPLE__
+        Handle<Value>* argv = new Handle<Value>[argc];
+    #else
+        Handle<Value> argv[argc];
+    #endif
 
     for (uint i = 0; i < argc; i++)
         argv[i] = GValueToV8 (isolate, &g_argv[i]);
 
     Handle<Object> this_obj = func;
     Handle<Value> return_value = func->Call (this_obj, argc, argv);
+
+    #ifdef __APPLE__
+        delete[] argv;
+    #endif
 
     if (g_return_value)
         V8ToGValue (g_return_value, return_value);
