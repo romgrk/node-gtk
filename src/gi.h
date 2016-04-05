b81d50a96e4060a9f6662c4c91bb4296078b0589
Rom Grk <romgrk.cc@gmail.com>
2016-04-04 23:14:33 -0400

    removed unused macros

--- src/gi.h
+++ src/gi.h
@@ -5,12 +5,14 @@
 #include <nan.h>
 #include <girepository.h>
 
-#define UTF8(s) String::NewFromUtf8 (isolate, s)
-#define FUNC(f) FunctionTemplate::New (isolate, f)->GetFunction ()
-#define EXPORT(name,value) exports->Set (String::NewFromUtf8 (isolate, name), value)
+#define UTF8(s)         String::NewFromUtf8 (isolate, s)
 
-#define THROW(error,...) \
-        isolate->ThrowException ( error (String::NewFromUtf8 (isolate, g_strdup_printf(__VA_ARGS__))));
+#define UTF8_NAME(s)    String::NewFromUtf8 (isolate, g_base_info_get_name(s))
 
-#define THROW_E(error, expr) \
-        isolate->ThrowException ( error (String::NewFromUtf8 (isolate, expr->message )));
+#define THROW(error,...)                    \
+        isolate->ThrowException ( error (   \
+                v8::String::NewFromUtf8 (isolate, g_strdup_printf(__VA_ARGS__))));
+
+#define THROW_E(error, expr)                \
+        isolate->ThrowException ( error (   \
+        v8::String::NewFromUtf8 (isolate, expr->message )));
