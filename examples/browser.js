ac84756e40331a3cb72befe6715f5581eabdd45d
Rom Grk <romgrk.cc@gmail.com>
2016-06-20 07:15:42 -0400

    style update

--- examples/browser.js
+++ examples/browser.js
@@ -20,13 +20,15 @@ ngtk.startLoop();
     }),
     // the WebKit2 browser wrapper
     webView = new WebKit2.WebView(),
+
+    webSettings = webView.getSettings(),
     // toolbar with buttons
     toolbar = new Gtk.Toolbar(),
     // buttons to go back, go forward, or refresh
     button = {
-      back: Gtk.ToolButton.new_from_stock(Gtk.STOCK_GO_BACK),
-      forward: Gtk.ToolButton.new_from_stock(Gtk.STOCK_GO_FORWARD),
-      refresh: Gtk.ToolButton.new_from_stock(Gtk.STOCK_REFRESH)
+      back: Gtk.ToolButton.newFromStock(Gtk.STOCK_GO_BACK),
+      forward: Gtk.ToolButton.newFromStock(Gtk.STOCK_GO_FORWARD),
+      refresh: Gtk.ToolButton.newFromStock(Gtk.STOCK_REFRESH)
     },
     // where the URL is written and shown
     urlBar = new Gtk.Entry(),
@@ -40,29 +42,32 @@ ngtk.startLoop();
   // Setting up optional Dark theme (gotta love it!)
   // ./browser.js google.com dark
   if (process.argv.some(color => color === 'dark')) {
-    let gtkSettings = Gtk.Settings.get_default();
-    gtkSettings.gtk_application_prefer_dark_theme = true;
-    gtkSettings.gtk_theme_name = 'Adwaita';
+    let gtkSettings = Gtk.Settings.getDefault();
+    // gtkSettings.gtkApplicationPreferDarkTheme = true;
+    // gtkSettings.gtkThemeName = 'Adwaita';
   }
+  webSettings.enableDeveloperExtras = true;
+  webSettings.enableCaretBrowsing = true;
+  console.log('webSettings: ', webSettings);
 
   // open first argument or Google
-  webView.load_uri(url(process.argv[2] || 'google.com'));
+  webView.loadUri(url(process.argv[2] || 'google.com'));
 
   // whenever a new page is loaded ...
   webView.connect('load-changed', (widget, load_event, data) => {
     switch (load_event) {
       case 2: // XXX: where is WEBKIT_LOAD_COMMITTED ?
         // ... update the URL bar with the current adress
-        urlBar.set_text(widget.get_uri());
-        button.back.set_sensitive(webView.can_go_back());
-        button.forward.set_sensitive(webView.can_go_forward());
+        urlBar.setText(widget.getUri());
+        button.back.setSensitive(webView.canGoBack());
+        button.forward.setSensitive(webView.canGoForward());
         break;
     }
   });
 
   // configure buttons actions
-  button.back.connect('clicked', () => webView.go_back());
-  button.forward.connect('clicked', () => webView.go_forward());
+  button.back.connect('clicked', () => webView.goBack());
+  button.forward.connect('clicked', () => webView.goForward());
   button.refresh.connect('clicked', () => webView.reload());
 
   // enrich the toolbar
@@ -73,28 +78,28 @@ ngtk.startLoop();
   // define "enter" / call-to-action event
   // whenever the url changes on the bar
   urlBar.connect('activate', () => {
-    let href = url(urlBar.get_text());
-    urlBar.set_text(href);
-    webView.load_uri(href);
+    let href = url(urlBar.getText());
+    urlBar.setText(href);
+    webView.loadUri(href);
   });
 
   // make the container scrollable
   scrollWindow.add(webView);
 
   // pack horizontally toolbar and url bar
-  hbox.pack_start(toolbar, false, false, 0);
-  hbox.pack_start(urlBar, true, true, 8);
+  hbox.packStart(toolbar, false, false, 0);
+  hbox.packStart(urlBar, true, true, 8);
 
   // pack vertically top bar (hbox) and scrollable window
-  vbox.pack_start(hbox, false, true, 0);
-  vbox.pack_start(scrollWindow, true, true, 0);
+  vbox.packStart(hbox, false, true, 0);
+  vbox.packStart(scrollWindow, true, true, 0);
 
   // configure main window
-  window.set_default_size(1024, 720);
-  window.set_resizable(true);
+  window.setDefaultSize(1024, 720);
+  window.setResizable(true);
   window.connect('show', () => {
     // bring it on top in OSX
-    window.set_keep_above(true);
+    // window.setKeepAbove(true);
     Gtk.main()
   });
   window.connect('destroy', () => Gtk.main_quit());
@@ -102,7 +107,7 @@ ngtk.startLoop();
 
   // add vertical ui and show them all
   window.add(vbox);
-  window.show_all();
+  window.showAll();
 
   // little helper
   // if link doesn't have a protocol, prefixes it via http://
@@ -111,6 +116,6 @@ ngtk.startLoop();
   }
 
 }(
-  ngtk.importNS('Gtk'),
+  ngtk.importNS('Gtk', '3.0'),
   ngtk.importNS('WebKit2')
 ));
