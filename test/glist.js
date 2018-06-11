9c27503c6f85562993cd06931876e6307a863f5f
Rom Grk <romgrk.cc@gmail.com>
2018-06-11 15:32:27 -0400

    add test for GList

---
+++ test/glist.js
@@ -0,0 +1,49 @@
+/*
+ * glist.js
+ */
+
+const gi = require('../lib/')
+
+const Gtk = gi.require('Gtk', '3.0')
+
+gi.startLoop()
+Gtk.init(null, 0)
+
+const window = new Gtk.Window({
+  type : Gtk.WindowType.TOPLEVEL
+})
+
+const flowBox = new Gtk.FlowBox()
+
+const button = {
+  back: Gtk.ToolButton.newFromStock(Gtk.STOCK_GO_BACK),
+  forward: Gtk.ToolButton.newFromStock(Gtk.STOCK_GO_FORWARD),
+  refresh: Gtk.ToolButton.newFromStock(Gtk.STOCK_REFRESH),
+}
+
+// where the URL is written and shown
+const urlBar = new Gtk.Entry()
+
+flowBox.add(button.back)
+flowBox.add(button.forward)
+flowBox.add(button.refresh)
+flowBox.add(urlBar)
+
+// configure main window
+window.setDefaultSize(400, 100)
+window.setResizable(true)
+window.connect('show', () => {
+  setTimeout(() => {
+    const list = flowBox.getSelectedChildren()
+    console.log(list)
+    Gtk.main_quit()
+  }, 50)
+  Gtk.main()
+})
+
+window.connect('destroy', () => Gtk.main_quit())
+window.connect('delete_event', () => false)
+
+// add vertical ui and show them all
+window.add(flowBox)
+window.showAll()
