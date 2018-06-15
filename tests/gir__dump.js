10e290c926948a12d23af3f6050a4eb4d3926e20
Rom Grk <romgrk.cc@gmail.com>
2018-06-15 17:32:16 -0400

    refactor inspect.js

---
+++ tests/gir__dump.js
@@ -0,0 +1,9 @@
+/*
+ * dump.js
+ */
+
+const inspect = require('../lib/inspect.js')
+
+const gi  = inspect.parseNamespace('GIRepository', '2.0')
+const gtk = inspect.parseNamespace('Gtk', '3.0')
+const gdk = inspect.parseNamespace('Gdk', '3.0')
