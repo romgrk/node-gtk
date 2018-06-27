6cceecb76d865424acff1cc69ec076da50b08582
Rom Grk <romgrk.cc@gmail.com>
2018-06-26 22:06:10 -0400

    fix test

--- tests/function_call__throws_missing_args.js
+++ tests/function_call__throws_missing_args.js
@@ -1,13 +1,18 @@
 /*
  * function_call__throws_missing_args.js
  */
-/* global test, expect */
 
 const gi = require('../')
 const Gtk = gi.require('Gtk', '3.0');
 
-test('fills caller-allocated arguments', () => {
-  expect(() => {
-    Gtk.accelerator_get_label();
-  }).toThrow();
-})
+let didThrow = false
+
+try {
+  Gtk.accelerator_get_label()
+} catch (e) {
+  didThrow = true
+  console.assert(e instanceof TypeError)
+  console.log('Got expected error:', e.message)
+}
+
+console.assert(didThrow)
