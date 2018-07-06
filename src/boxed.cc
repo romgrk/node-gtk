a7e87e9ebb009cca8c3a9aaad658decc41868d90
Rom Grk <romgrk.cc@gmail.com>
2018-07-06 01:05:34 -0400

    boxed.cc: clean

--- src/boxed.cc
+++ src/boxed.cc
@@ -20,87 +20,12 @@ using v8::Local;
 using v8::Number;
 using v8::Object;
 using v8::String;
-//using v8::WeakCallbackInfo;
 using v8::Persistent;
 using Nan::New;
 using Nan::WeakCallbackType;
 
 namespace GNodeJS {
 
-// https://github.com/TooTallNate/node-weak/blob/master/src/weakref.cc
-
-/*static void InitBoxedFromStruct (Local<Object> self, GIStructInfo *info) {
- *    gpointer pointer = self->GetAlignedPointerFromInternalField(0);
- *
- *    int n_fields = g_struct_info_get_n_fields(info);
- *    for (int i = 0; i < n_fields; i++) {
- *        GIFieldInfo *field    = g_struct_info_get_field(info, i);
- *        const char *fieldName = g_base_info_get_name(field);
- *        GIArgument value;
- *
- *        if (g_field_info_get_field(field, pointer, &value)) {
- *            GITypeInfo  *fieldType = g_field_info_get_type(field);
- *            char *_name = g_strdup_printf("_%s", fieldName);
- *
- *            self->Set(
- *                UTF8(_name),
- *                GIArgumentToV8(fieldType, &value));
- *
- *            g_base_info_unref (fieldType);
- *            g_free(_name);
- *        }
- *
- *        g_base_info_unref (field);
- *    }
- *}
-
-static void InitBoxedFromUnion (Local<Object> self, GIUnionInfo *info) {
-    void *boxed = self->GetAlignedPointerFromInternalField(0);
-
-    int n_fields = g_union_info_get_n_fields(info);
-    for (int i = 0; i < n_fields; i++) {
-        GIArgument   value;
-        GIFieldInfo *field = g_union_info_get_field(info, i);
-
-        bool success = g_field_info_get_field(field, boxed, &value);
-
-        if (success) {
-            GITypeInfo  *field_type = g_field_info_get_type(field);
-            const char  *fieldName  = g_base_info_get_name(field);
-
-            self->Set( UTF8(fieldName), GIArgumentToV8(field_type, &value));
-
-            GIStructInfo *union_type;
-            GIFieldInfo *union_field;
-            GITypeInfo *union_info;
-
-            union_field = g_union_info_get_field(info, value.v_int);
-            union_info  = g_field_info_get_type(union_field);
-
-            GITypeTag tag = g_type_info_get_tag(union_info);
-
-            if (tag == GI_TYPE_TAG_INTERFACE) {
-                union_type  = g_type_info_get_interface(union_info);
-                GType gtype = g_registered_type_info_get_g_type(union_type);
-
-                auto tpl = GetBoxedTemplate(union_type, gtype);
-                auto p_tpl = tpl->PrototypeTemplate();
-
-                Nan::Set(self, UTF8("__p_tpl__"), p_tpl->NewInstance());
-
-                g_base_info_unref(union_type);
-            }
-
-            g_base_info_unref(union_info);
-            g_base_info_unref(union_field);
-            g_base_info_unref(field_type);
-        }
-
-        g_base_info_unref(field);
-        //if (success) break;
-    }
-}
- */
 
 size_t Boxed::GetSize (GIBaseInfo *boxed_info) {
     GIInfoType i_type = g_base_info_get_type(boxed_info);
@@ -133,12 +58,7 @@ static void BoxedConstructor(const Nan::FunctionCallbackInfo<Value> &args) {
         /* The External case. This is how WrapperFromBoxed is called. */
 
         boxed = External::Cast(*args[0])->Value();
-
         self->SetAlignedPointerInInternalField (0, boxed);
-
-        // TODO: this
-        // if (type == GI_INFO_TYPE_UNION)
-            // g_warning("BoxedConstructor: union gi_info: %s", g_base_info_get_name(gi_info));
 
     } else {
         /* User code calling `new Pango.AttrList()` */
@@ -183,55 +103,6 @@ static void BoxedDestroyed(const Nan::WeakCallbackInfo<Boxed> &info) {
     delete box;
 }
 
-void FieldGetter(Local<String> property, Nan::NAN_GETTER_ARGS_TYPE info) {
-    // Local<v8::String> property
-    Local<Object> self = info.This();
-    External     *data = External::Cast(*info.Data());
-    void *boxed = self->GetAlignedPointerFromInternalField(0);
-
-    if (boxed == NULL) {
-        g_warning("FieldGetter: NULL boxed pointer");
-        return;
-    }
-
-    GIArgument value;
-    GIFieldInfo *field = static_cast<GIFieldInfo*>(data->Value());
-
-    g_assert(field);
-
-    if (g_field_info_get_field(field, boxed, &value)) {
-        GITypeInfo  *field_type = g_field_info_get_type(field);
-        info.GetReturnValue().Set(GIArgumentToV8(field_type, &value));
-        g_base_info_unref (field_type);
-
-        // GNodeJS::reeGIArgument(field_type, &value);
-    } else {
-        g_warning ("FieldGetter: couldnt get field %s", g_base_info_get_name(field));
-        g_warning ("FieldGetter: property name: %s", *Nan::Utf8String(property) );
-    }
-}
-
-void FieldSetter(Local<String> property,
-                 Local<Value> value,
-                 Nan::NAN_SETTER_ARGS_TYPE info) {
-
-    Local<Object> self = info.This();
-    void *boxed = self->GetAlignedPointerFromInternalField(0);
-    GIFieldInfo *field = static_cast<GIFieldInfo*>(External::Cast(*info.Data())->Value());
-    GITypeInfo  *field_type = g_field_info_get_type(field);
-
-    GIArgument arg;
-
-    if (V8ToGIArgument(field_type, &arg, value, true)) {
-        if (g_field_info_set_field(field, boxed, &arg) == FALSE)
-            g_warning ("FieldSetter: couldnt set field %s", g_base_info_get_name(field));
-    } else {
-        g_warning ("FieldSetter: couldnt convert value for field %s", g_base_info_get_name(field));
-    }
-
-    g_base_info_unref (field_type);
-}
-
 
 Local<FunctionTemplate> GetBoxedTemplate(GIBaseInfo *info, GType gtype) {
     void *data = NULL;
@@ -258,29 +129,6 @@ Local<FunctionTemplate> GetBoxedTemplate(GIBaseInfo *info, GType gtype) {
     } else {
         const char *class_name = g_base_info_get_name (info);
         tpl->SetClassName( UTF8(class_name) );
-    }
-
-    if (GI_IS_STRUCT_INFO(info)) {
-        //int n_fields = g_struct_info_get_n_fields(info);
-        //for (int i = 0; i < n_fields; i++) {
-            //GIFieldInfo *field = g_struct_info_get_field(info, i);
-            //const char  *name  = g_base_info_get_name(field);
-            ////char *jsName = Util::toCamelCase(name);
-            //Nan::SetAccessor( tpl->InstanceTemplate(),
-                    //UTF8(name), //UTF8(jsName),
-                    //FieldGetter, //FieldSetter,
-                    //Nan::New<External>(field));
-            ////g_base_info_unref (field);
-        //}
-
-    } else if (GI_IS_UNION_INFO(info)) {
-        // XXX this
-
-    } else if (g_base_info_get_type (info) == GI_INFO_TYPE_BOXED) {
-        // XXX this
-
-    } else {
-        g_assert_not_reached ();
     }
 
     if (gtype == G_TYPE_NONE)
