37ef8b56629443f4b945db0b6ca01176360f2c48
Rom Grk <romgrk.cc@gmail.com>
2018-07-17 17:01:51 -0400

    tests: update loop test to fail

--- tests/loop.js
+++ tests/loop.js
@@ -9,15 +9,25 @@ const Gtk = gi.require('Gtk', '3.0')
 gi.startLoop()
 Gtk.init()
 
+let didCallTimeout = false
+let didCallPromise = false
+
 setTimeout(() => {
   console.log('timeout')
-}, 1000)
+  didCallTimeout = true
+}, 500)
 
 const promise = new Promise(resolve => resolve('promise resolved'))
-promise.then(result => console.log(result))
+promise.then(result => {
+  console.log(result)
+  didCallPromise = true
+})
 
 setTimeout(() => {
-  console.log('quit')
   Gtk.mainQuit()
-}, 5000)
+  console.assert(didCallTimeout, 'did not call timeout')
+  console.assert(didCallPromise, 'did not call promise')
+  console.log('done')
+}, 1000)
+
 Gtk.main()
