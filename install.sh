fdf627c739aa4b738b8db0d621b1e0113a9b38a4
Rom Grk <romgrk.cc@gmail.com>
2018-07-17 18:27:00 -0400

    make node-pre-gyp work

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
 #! /bin/sh
 # install.sh
-# Copyright (C) 2018 rgregoir <rgregoir@laurier>
+# Copyright (C) 2018 romgrk <romgrk.cc@gmail.com>
 #
 
 if [ "$(uname)" = "Darwin" ] && [ "$(which brew)" != "" ]; then
     export PKG_CONFIG_PATH=$(brew --prefix libffi)/lib/pkgconfig
 fi
 
-node-pre-gyp install
-
-if [ -n $? ]; then
-    node-gyp configure
-    node-gyp build
-fi
+node-pre-gyp install --fallback-to-build
