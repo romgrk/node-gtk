e25764003bc1fab7e54992a6974d6fa9d45b5c00
Rom Grk <romgrk.cc@gmail.com>
2018-07-18 19:55:33 -0400

    refactor value.cc

--- src/value.cc
+++ src/value.cc
@@ -593,44 +593,7 @@ item_error:
     return hash_table;
 }
 
-bool V8ToGIArgument(GIBaseInfo *gi_info, GIArgument *arg, Local<Value> value) {
-    GIInfoType type = g_base_info_get_type (gi_info);
-
-    switch (type) {
-    case GI_INFO_TYPE_BOXED:
-    case GI_INFO_TYPE_STRUCT:
-    case GI_INFO_TYPE_UNION:
-        arg->v_pointer = BoxedFromWrapper(value);
-        break;
-
-    case GI_INFO_TYPE_FLAGS:
-    case GI_INFO_TYPE_ENUM:
-        arg->v_int = value->Int32Value ();
-        break;
-
-    case GI_INFO_TYPE_OBJECT:
-    {
-        GType gtype = g_registered_type_info_get_g_type (gi_info);
-
-        if (g_type_is_a(gtype, G_TYPE_PARAM)) {
-            arg->v_pointer = ParamSpec::FromWrapper(value);
-            break;
-        }
-        // fallthrough
-    }
-    case GI_INFO_TYPE_INTERFACE:
-        arg->v_pointer = GObjectFromWrapper(value);
-        break;
-
-    case GI_INFO_TYPE_CALLBACK:
-    default:
-        print_info (gi_info);
-        g_assert_not_reached ();
-    }
-    return true;
-}
-
-bool V8ToGIArgument(GITypeInfo *type_info, GIArgument *arg, Local<Value> value, bool may_be_null) {
+bool V8ToGIArgument(GITypeInfo *type_info, GIArgument *arg, Local<Value> value, bool may_be_null, GIArgInfo* arg_info) {
     GITypeTag type_tag = g_type_info_get_tag (type_info);
 
     if (value->IsUndefined () || value->IsNull ()) {
@@ -720,7 +683,43 @@ bool V8ToGIArgument(GITypeInfo *type_info, GIArgument *arg, Local<Value> value, 
     case GI_TYPE_TAG_INTERFACE:
         {
             GIBaseInfo *interface_info = g_type_info_get_interface (type_info);
-            V8ToGIArgument (interface_info, arg, value);
+            GIInfoType interface_type = g_base_info_get_type (interface_info);
+
+            switch (interface_type) {
+            case GI_INFO_TYPE_BOXED:
+            case GI_INFO_TYPE_STRUCT:
+            case GI_INFO_TYPE_UNION:
+                arg->v_pointer = BoxedFromWrapper(value);
+                break;
+
+            case GI_INFO_TYPE_FLAGS:
+            case GI_INFO_TYPE_ENUM:
+                arg->v_int = value->Int32Value ();
+                break;
+
+            case GI_INFO_TYPE_OBJECT:
+            {
+                GType gtype = g_registered_type_info_get_g_type (interface_info);
+
+                if (g_type_is_a(gtype, G_TYPE_PARAM)) {
+                    arg->v_pointer = ParamSpec::FromWrapper(value);
+                    break;
+                }
+                // fallthrough
+            }
+            case GI_INFO_TYPE_INTERFACE:
+                arg->v_pointer = GObjectFromWrapper(value);
+                break;
+
+            case GI_INFO_TYPE_CALLBACK:
+                arg->v_pointer = nullptr;
+                break;
+
+            default:
+                print_info (interface_info);
+                g_assert_not_reached ();
+            }
+
             g_base_info_unref(interface_info);
         }
         break;
@@ -811,6 +810,8 @@ bool CanConvertV8ToGIArgument(GITypeInfo *type_info, Local<Value> value, bool ma
                 result = true;
                 break;
             case GI_INFO_TYPE_CALLBACK:
+                result = value->IsFunction();
+                break;
             default:
                 print_info (interface_info);
                 g_assert_not_reached ();
--- src/value.h
+++ src/value.h
@@ -17,8 +17,7 @@ Local<Value> GHashToV8 (GITypeInfo *info, GHashTable *hash);
 Local<Value> ArrayToV8  (GITypeInfo *info, gpointer data, int length = -1);
 Local<Value> GIArgumentToV8 (GITypeInfo *type_info, GIArgument *argument, int length = -1);
 
-bool         V8ToGIArgument (GITypeInfo *type_info, GIArgument *argument, Local<Value> value);
-bool         V8ToGIArgument (GITypeInfo *type_info, GIArgument *argument, Local<Value> value, bool may_be_null);
+bool         V8ToGIArgument (GITypeInfo *type_info, GIArgument *argument, Local<Value> value, bool may_be_null = false, GIArgInfo* arg_info = nullptr);
 void         FreeGIArgument (GITypeInfo *type_info, GIArgument *argument, GITransfer transfer = GI_TRANSFER_EVERYTHING, GIDirection direction = GI_DIRECTION_OUT);
 void         FreeGIArgumentArray (GITypeInfo *type_info, GIArgument *arg, GITransfer transfer = GI_TRANSFER_EVERYTHING, GIDirection direction = GI_DIRECTION_OUT, int length = -1);
 bool         CanConvertV8ToGIArgument (GITypeInfo *type_info, Local<Value> value, bool may_be_null);
