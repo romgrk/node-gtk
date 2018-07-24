54c400342278b05bf1465910600dab8a82ea3ef6
Rom Grk <romgrk.cc@gmail.com>
2018-07-24 16:47:14 -0400

    travis: add build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+source ~/.nvm/nvm.sh
+
+set -e -u
+
+PUBLISH_BINARIES=false;
+REPUBLISH_BINARIES=false;
+
+if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
+    PUBLISH_BINARIES=true;
+fi;
+if [[ ${COMMIT_MESSAGE} =~ "[republish binary]" ]]; then
+    REPUBLISH_BINARIES=true;
+fi;
+
+
+function publish() {
+    if [[ $PUBLISH_BINARIES == true ]]; then
+        node-pre-gyp package testpackage;
+        node-pre-gyp publish;
+        node-pre-gyp info;
+    elif [[ $REPUBLISH_BINARIES == true ]]; then
+        node-pre-gyp package testpackage;
+        node-pre-gyp republish;
+        node-pre-gyp info;
+    fi;
+}
+
+# test installing from source
+if [[ $PUBLISH_BINARIES == false ]] && [[ $REPUBLISH_BINARIES == false ]]; then
+    npm install --build-from-source
+    npm test
+else
+    echo "Building binaries for publishing"
+    npm install --build-from-source
+    npm test
+fi
