429f64ec0ef5063be8f0dec85882f35362597f7b
Rom Grk <romgrk.cc@gmail.com>
2019-05-17 17:13:29 -0400

    tests: add regression__gdkRgba_field_access.js

---
+++ tests/regression__gdkRgba_field_access.js
@@ -0,0 +1,35 @@
+/*
+ * regression__gdkRgba_field_access.js
+ */
+
+
+const gi = require('../');
+const Gtk = gi.require('Gtk');
+const Pango = gi.require('Pango');
+const common = require('./__common__.js')
+
+gi.startLoop()
+Gtk.init()
+
+
+// common.skip()
+
+const window = new Gtk.Window({ type : Gtk.WindowType.TOPLEVEL })
+window.setDefaultSize(200, 200)
+window.setResizable(true)
+
+window.on('show', () => {
+  setTimeout(run, 500)
+  Gtk.main()
+})
+window.on('destroy', () => Gtk.mainQuit())
+window.on('delete-event', () => false)
+
+window.showAll()
+
+function run() {
+  const color = window.getStyleContext().getBackgroundColor(Gtk.StateFlags.NORMAL)
+  console.log(color.red)
+  console.log(color.toString())
+  window.close()
+}
