50d9fec4b579f21e2c873487a82cf164e22ab3b0
Rom Grk <romgrk.cc@gmail.com>
2020-05-28 23:32:39 -0400

    tests: proper caller-allocation testing

--- tests/function_call__caller_allocated.js
+++ tests/function_call__caller_allocated.js
@@ -6,9 +6,22 @@ const gi = require('../')
 const Gtk = gi.require('Gtk', '3.0')
 const common = require('./__common__.js')
 
+Gtk.init()
+
+const name = 'custom-mark'
+
 const buffer = new Gtk.TextBuffer()
+const mark = new Gtk.TextMark({ name })
 const iter = buffer.getStartIter(/* TextIter */)
+
 common.assert(iter !== undefined, 'iter !== undefined')
 common.assert(iter.__proto__ === Gtk.TextIter.prototype, 'iter.__proto__ === Gtk.TextIter.prototype')
 common.assert(iter instanceof Gtk.TextIter, 'iter instanceof Gtk.TextIter')
-console.log('Success:', iter)
+
+buffer.addMark(mark, iter)
+
+const returnedMark = buffer.getMark(name)
+
+common.assert(mark === returnedMark, 'mark === returnedMark')
+
+console.log('Success:', iter, returnedMark)
