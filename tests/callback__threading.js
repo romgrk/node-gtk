bc8ca819c1be345fc555cda9d1996f9293aa3aba
Nico Strasdat <nstrasdat@gmail.com>
2020-06-22 13:08:47 +0200

    Implement another test for callback from another thread

--- tests/callback__threading.js
+++ tests/callback__threading.js
@@ -1,19 +1,29 @@
 const gi = require('../lib/')
-const GLib = gi.require('GLib', '2.0')
-const Gio = gi.require('Gio', '2.0')
+const Gst = gi.require('Gst')
 const common = require('./__common__.js')
-
 gi.startLoop()
+Gst.init()
 
 common.describe('callback should be called from another thread', async () => {
-  await new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
+    const pipeline = new Gst.Pipeline()
+    const webrtcbin = Gst.ElementFactory.make('webrtcbin')
+  
+    if (!pipeline || !webrtcbin) {
+      throw new Error('Could not create all elements.')
+    }
+    pipeline.add(webrtcbin)
+  
+    pipeline.setState(Gst.State.PLAYING)
+  
     const timeout = setTimeout(() => {
-      reject('timeout')
-    }, 1000)
-    const task = new Gio.Task(null)
-    task.runInThread(() => {
-      clearTimeout(timeout)
+      reject()
+    }, 500)
+    const s = Gst.Structure.newEmpty('structure')
+    const p = Gst.Promise.newWithChangeFunc(() => {
       resolve()
+      clearTimeout(timeout)
     })
+    webrtcbin.emit('create-offer', s, p)
   })
 })
