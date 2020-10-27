5200c8a02a5ca852cca2657a5fe1a77a61e27b89
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2020-10-27 10:15:04 +0100

    Add test for GError

---
+++ tests/error.js
@@ -0,0 +1,30 @@
+/*
+ * error.js
+ */
+
+
+const gi = require('../lib/')
+const GLib = gi.require('GLib', '2.0')
+const Gst = gi.require('Gst', '1.0')
+const common = require('./__common__.js')
+
+Gst.init()
+
+const ERROR_MESSAGE = 'ERROR_MESSAGE'
+const ERROR_CODE = 42
+const DEBUG_STRING = 'debug string'
+
+common.describe('GError', () => {
+  common.it('should be returned', () => {
+    common.assert(GLib.Error !== undefined)
+    const err0 = GLib.Error.newLiteral(404, ERROR_CODE, ERROR_MESSAGE)
+    common.assert(err0.code === ERROR_CODE && err0.message === ERROR_MESSAGE)
+    console.log(err0)
+
+    const msg = Gst.Message.newError(null, err0, DEBUG_STRING)
+    const [err1, debugString] = msg.parseError();
+    console.log(err1)
+    common.assert(err1.code === ERROR_CODE && err1.message === ERROR_MESSAGE)
+    common.assert(debugString === DEBUG_STRING)
+  })
+})
