7153e4949074244f1b1eb70248eeb9acf736c410
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2020-10-28 09:01:49 +0100

    Fix handling of GQuarks

--- tests/error.js
+++ tests/error.js
@@ -10,6 +10,8 @@ const common = require('./__common__.js')
 
 Gst.init()
 
+const QUARK_STRING = 'example'
+const CUSTOM_QUARK = GLib.quarkFromString(QUARK_STRING)
 const ERROR_MESSAGE = 'ERROR_MESSAGE'
 const ERROR_CODE = 42
 const DEBUG_STRING = 'debug string'
@@ -17,12 +19,13 @@ const DEBUG_STRING = 'debug string'
 common.describe('GError', () => {
   common.it('should be returned', () => {
     common.assert(GLib.Error !== undefined)
-    const err0 = GLib.Error.newLiteral(404, ERROR_CODE, ERROR_MESSAGE)
+    const err0 = GLib.Error.newLiteral(CUSTOM_QUARK, ERROR_CODE, ERROR_MESSAGE)
     common.assert(err0.code === ERROR_CODE && err0.message === ERROR_MESSAGE)
 
     const msg = Gst.Message.newError(null, err0, DEBUG_STRING)
     const [err1, debugString] = msg.parseError();
     common.assert(err1.code === ERROR_CODE && err1.message === ERROR_MESSAGE)
     common.assert(debugString === DEBUG_STRING)
+    common.assert(GLib.quarkToString(err1.domain) === QUARK_STRING)
   })
 })
