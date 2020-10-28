138141825464989549781186a2c1d2933b5a4efb
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2020-10-28 10:07:39 +0100

    Rewrite error test again

--- tests/error.js
+++ tests/error.js
@@ -6,7 +6,7 @@
 const gi = require('../lib/')
 const GLib = gi.require('GLib', '2.0')
 const Gst = gi.require('Gst', '1.0')
-const { describe, it, assert } = require('./__common__.js')
+const { describe, it, assert, expect } = require('./__common__.js')
 
 Gst.init()
 
@@ -17,15 +17,25 @@ const ERROR_CODE = 42
 const DEBUG_STRING = 'debug string'
 
 describe('GError', () => {
-  it('should be returned', () => {
+  it('should exist', () => {
     assert(GLib.Error !== undefined, 'Error is not available')
-    const err0 = GLib.Error.newLiteral(CUSTOM_QUARK, ERROR_CODE, ERROR_MESSAGE)
-    assert(err0.code === ERROR_CODE && err0.message === ERROR_MESSAGE, 'code or message is not accessible')
+  })
 
+  it('should be created from literal', () => {
+    const err = GLib.Error.newLiteral(CUSTOM_QUARK, ERROR_CODE, ERROR_MESSAGE)
+    expect(err.domain, CUSTOM_QUARK)
+    expect(err.code, ERROR_CODE)
+    expect(err.message, ERROR_MESSAGE)
+  })
+
+  it('should be returned from parseError', () => {
+    const err0 = GLib.Error.newLiteral(CUSTOM_QUARK, ERROR_CODE, ERROR_MESSAGE)
     const msg = Gst.Message.newError(null, err0, DEBUG_STRING)
-    const [err1, debugString] = msg.parseError();
-    assert(err1.code === ERROR_CODE && err1.message === ERROR_MESSAGE, 'code or message is not equal')
-    assert(debugString === DEBUG_STRING, 'debug string is not returned')
-    assert(err1.domain === CUSTOM_QUARK && GLib.quarkToString(err1.domain) === QUARK_STRING, 'domain does not match')
+    const [err1, debugString] = msg.parseError()
+    expect(err1.domain, CUSTOM_QUARK)
+    expect(err1.code, ERROR_CODE)
+    expect(err1.message, ERROR_MESSAGE)
+    expect(debugString, DEBUG_STRING)
+    expect(GLib.quarkToString(err1.domain), QUARK_STRING)
   })
 })
