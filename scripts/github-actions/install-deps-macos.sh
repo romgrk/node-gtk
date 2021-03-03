ed38def42963b7b99de047d637a89eb1cc57e3b1
Rom Grk <romgrk.cc@gmail.com>
2021-03-03 02:28:57 -0500

    build: initial gha config

---
+++ scripts/github-actions/install-deps-macos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew install \
+    gtk+3 \
+    gobject-introspection \
+    glib \
+    libsoup \
+    cairo \
+    gstreamer \
+    gst-plugins-base \
+    gst-plugins-good \
+    gst-plugins-bad \
+    libnice
