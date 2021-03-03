816ec8db7aa74c848184c9d996c74c3d3e82cbac
Rom Grk <romgrk.cc@gmail.com>
2021-03-03 03:15:54 -0500

    build: update gha config

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -49,11 +49,9 @@ function npm_test() {
 
 # test installing from source
 if [[ $PUBLISH_BINARIES == false ]] && [[ $REPUBLISH_BINARIES == false ]]; then
-    npm install --build-from-source
     npm_test
 else
     echo "### Building binaries for publishing ###"
-    npm install --build-from-source
     npm_test
     publish
 fi
