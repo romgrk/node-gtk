bd7dc33642ad06671ff1d389d749a33e21333f4e
Rom Grk <romgrk.cc@gmail.com>
2021-03-07 05:05:11 -0500

    tests: implement loop testing

--- tests/loop.js
+++ tests/loop.js
@@ -1,34 +1,119 @@
 /*
  * loop.js
+ *
+ * Promise & setTimeout not run when inside signal callback
+ * https://github.com/romgrk/node-gtk/issues/121
  */
 
+const gi = require('..')
+const GLib = gi.require('GLib', '2.0')
+const Gtk = gi.require('Gtk', '4.0')
+const { expect, assert } = require('./__common__')
 
-const gi = require('../lib/')
-const Gtk = gi.require('Gtk', '3.0')
-const common = require('./__common__.js')
+const events = []
+const expectedEvents = [
+  'realize',
+  'clicked',
+  'promise:start-before',
+  'clicked:settimeout',
+  'clicked:promise-settimeout',
+  'clicked:promise-resolved',
+]
 
-gi.startLoop()
-Gtk.init()
 
-let didCallTimeout = false
-let didCallPromise = false
+const loop = GLib.MainLoop.new(null, false)
+const app = new Gtk.Application('com.github.romgrk.node-gtk.demo', 0)
+app.on('activate', onActivate)
 
-setTimeout(() => {
-  console.log('timeout')
-  didCallTimeout = true
-}, 500)
+let window
+let actionButton
 
-const promise = new Promise(resolve => resolve('promise resolved'))
-promise.then(result => {
-  console.log(result)
-  didCallPromise = true
+
+let resolve
+const action = new Promise(r => { resolve = r })
+action.then(() => {
+  events.push('promise:start-before')
 })
 
 setTimeout(() => {
-  Gtk.mainQuit()
-  common.assert(didCallTimeout, 'did not call timeout')
-  common.assert(didCallPromise, 'did not call promise')
-  console.log('done')
-}, 1000)
+  if (window) window.close()
+  assert(false, 'Timeout should not be reached')
+}, 2000).unref()
 
-Gtk.main()
+const status = app.run([])
+
+console.log('Finished with status:', status)
+console.log(events)
+assert(
+  events.every((e, i) => e === expectedEvents[i]),
+  `Wrong events: ${JSON.stringify(events)}`
+)
+
+function onActivate() {
+  window = new Gtk.ApplicationWindow(app)
+  window.setTitle('Window')
+  window.setDefaultSize(200, 200)
+  window.on('close-request', onQuit)
+  window.on('realize', onRealize)
+
+  const ui = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <interface>
+      <requires lib="gtk" version="4.0"/>
+      <object class="GtkBox" id="root">
+        <property name="orientation">vertical</property>
+        <child>
+          <object class="GtkButton" id="actionButton">
+            <property name="label" translatable="yes">Action</property>
+            <property name="receives_default">1</property>
+          </object>
+        </child>
+      </object>
+    </interface>
+  `
+
+  const builder = Gtk.Builder.newFromString(ui, ui.length)
+  const root = builder.getObject('root')
+
+  actionButton = builder.getObject('actionButton')
+  actionButton.on('clicked', () => {
+    events.push('clicked')
+
+    // this runs, and promise is resolved
+    let action = new Promise(resolve => {
+      setTimeout(
+        () => {
+          events.push('clicked:promise-settimeout')
+          resolve()
+        },
+        100
+      )
+    })
+
+    setTimeout(() => {
+      events.push('clicked:settimeout')
+    }, 50)
+
+    action.then(() => {
+      events.push('clicked:promise-resolved')
+      window.close()
+    })
+  })
+
+  window.setChild(root)
+  window.present()
+
+  resolve()
+  gi.startLoop()
+  loop.run()
+}
+
+function onRealize() {
+  events.push('realize')
+  actionButton.emit('clicked')
+}
+
+function onQuit() {
+  loop.quit()
+  return false
+}
