b58b3a3d3715fb7c3a021fe5274201b3368a821c
Rom Grk <romgrk.cc@gmail.com>
2021-03-07 03:57:36 -0500

    docs: improve gtk4 example

--- examples/gtk-4.js
+++ examples/gtk-4.js
@@ -6,23 +6,14 @@ const gi = require('..')
 const GLib = gi.require('GLib', '2.0')
 const Gtk = gi.require('Gtk', '4.0')
 
-gi.startLoop()
-
 const printHello = () => console.log('Hello')
 
 const loop = GLib.MainLoop.new(null, false)
-
 const app = new Gtk.Application('com.github.romgrk.node-gtk.demo', 0)
 app.on('activate', onActivate)
-
 const status = app.run([])
 
 console.log('Finished with status:', status)
-
-function onQuit() {
-  loop.quit()
-  return false
-}
 
 function onActivate() {
   const window = new Gtk.ApplicationWindow(app)
@@ -30,12 +21,53 @@ function onActivate() {
   window.setDefaultSize(200, 200)
   window.on('close-request', onQuit)
 
-  const button = Gtk.Button.newWithLabel('Hello World')
-  button.on('clicked', printHello)
+  const ui = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <interface>
+      <requires lib="gtk" version="4.0"/>
+      <object class="GtkBox" id="root">
+        <property name="orientation">vertical</property>
+        <child>
+          <object class="GtkLabel" id="helloLabel">
+            <property name="vexpand">1</property>
+            <property name="label">Hello World!</property>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton" id="actionButton">
+            <property name="label" translatable="yes">Action</property>
+            <property name="receives_default">1</property>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton" id="closeButton">
+            <property name="label" translatable="yes">Close</property>
+            <property name="receives_default">1</property>
+          </object>
+        </child>
+      </object>
+    </interface>
+  `
 
-  window.setChild(button)
+  const builder = Gtk.Builder.newFromString(ui, ui.length)
+  const root = builder.getObject('root')
+
+  const actionButton = builder.getObject('actionButton')
+  actionButton.on('clicked', printHello)
+
+  const closeButton = builder.getObject('closeButton')
+  closeButton.on('clicked', () => window.close())
+
+  window.setChild(root)
   window.show()
   window.present()
 
+  gi.startLoop()
   loop.run()
 }
+
+function onQuit() {
+  loop.quit()
+  return false
+}
+
