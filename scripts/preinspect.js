26129e1f8c3af61b8ad0f0356f11a9068ce92d89
Rom Grk <romgrk.cc@gmail.com>
2021-04-07 03:09:10 -0400

    feat(preinspect): upgrade versions

--- scripts/preinspect.js
+++ scripts/preinspect.js
@@ -7,13 +7,10 @@
  */
 
 global.i = require('../lib/inspect')
-global.gtk = i.parseNamespace('Gtk', '3.0')
-global.gdk = i.parseNamespace('Gdk', '3.0')
+global.gtk = i.parseNamespace('Gtk', '4.0')
+global.gdk = i.parseNamespace('Gdk', '4.0')
 global.glib = i.parseNamespace('GLib', '2.0')
 global.go = i.parseNamespace('GObject', '2.0')
-global.json = i.parseNamespace('Json')
-global.soup = i.parseNamespace('Soup')
-global.gst = i.parseNamespace('Gst')
 
 // Example: find all inout arguments
 // as = i.infos.filter(i => i.infoType === 'arg' && i.direction === 'INOUT')
