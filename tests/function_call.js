691b0e8ff277fddc27cb828b68af85e369ae8046
Ratchanan Srirattanamet <ratchanan@ubports.com>
2022-01-09 06:07:36 +0700

    feat(value): accept BigInt for (u)int64 arguments (#322)
    This allows passing a large number (e.g. GST_CLOCK_TIME_NONE) to Gi.

--- tests/function_call.js
+++ tests/function_call.js
@@ -5,9 +5,11 @@
 const gi = require('../lib/')
 const GLib = gi.require('GLib')
 const Gtk = gi.require('Gtk', '3.0')
+const Gst = gi.require('Gst', '1.0')
 const { describe, it, mustThrow, expect } = require('./__common__.js')
 
 Gtk.init()
+Gst.init();
 
 describe('function arguments', () => {
   it('works', () => {
@@ -16,6 +18,20 @@ describe('function arguments', () => {
 
   it('works with null values', () => {
     // FIXME: find a test case for this with gimarshallingtest
+  })
+
+  it('works with bigint', () => {
+    // When converted to uint64, GST_CLOCK_TIME_NONE > Number.MAX_SAFE_INTEGER
+    const GST_CLOCK_TIME_NONE = BigInt.asUintN(/* width */ 64, -1n)
+
+    const elem = Gst.ElementFactory.make('identity', 'iden')
+
+    /*
+      typedef guint64 GstClockTime;
+      gst_element_set_start_time (GstElement * element,
+                                  GstClockTime time);
+    */
+    elem.setStartTime(GST_CLOCK_TIME_NONE)
   })
 })
 
