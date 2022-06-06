be1d42568755619849e7eaca9afc1c34d349b5f2
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-06 17:38:17 +0300

    Remove GCC attribute MSVC doesn't know
    At some point something like
    https://stackoverflow.com/questions/4226308/msvc-equivalent-of-attribute-warn-unused-result/22759336#22759336
    should be used

--- src/value.h
+++ src/value.h
@@ -26,7 +26,7 @@ void         FreeGIArgument (GITypeInfo *type_info, GIArgument *argument, GITran
 void         FreeGIArgumentArray (GITypeInfo *type_info, GIArgument *arg, GITransfer transfer = GI_TRANSFER_EVERYTHING, GIDirection direction = GI_DIRECTION_OUT, long length = -1);
 bool         CanConvertV8ToGIArgument (GITypeInfo *type_info, Local<Value> value, bool may_be_null);
 
-bool         V8ToGValue(GValue *gvalue, Local<Value> value, bool mustCopy = false) __attribute__((warn_unused_result));
+bool         V8ToGValue(GValue *gvalue, Local<Value> value, bool mustCopy = false);
 Local<Value> GValueToV8(const GValue *gvalue, bool mustCopy = false);
 bool         CanConvertV8ToGValue(GValue *gvalue, Local<Value> value);
 
